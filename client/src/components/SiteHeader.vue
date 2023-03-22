ffaaf4ddd8307a26518412b3638bdf699e8548c8
yzhsU <yzhs.go@gmail.com>
2023-03-22 14:40:34 +0900

    Write Css parameters of index header

--- client/src/components/SiteHeader.vue
+++ client/src/components/SiteHeader.vue
@@ -1,77 +1,97 @@
 <template>
-    <header>
-      <div class="header-container">
-        <div>
-          <img class="logo" src="images/logo.png"/>
-        </div>
-        <div>
-          <nav>
-            <ul>
-              <li><a href="#">킨더플레이룸</a>
-                <div class="sub-menu">
-    <ul>
-      <li>Menu 1</li>
-      <li>Menu 2</li>
-      <li>Menu 3</li>
-    </ul>
-  </div>
-              </li>
-              <li><a href="#">프랜차이즈</a>
-                <div class="sub-menu">
-    <ul>
-      <li>Menu 1</li>
-      <li>Menu 2</li>
-      <li>Menu 3</li>
-    </ul>
-  </div>
-              </li>
-              <li><a href="#">News</a>
-                <div class="sub-menu">
-    <ul>
-      <li>Menu 1</li>
-      <li>Menu 2</li>
-      <li>Menu 3</li>
-    </ul>
-  </div>
-              </li>
-              <li><a href="#">디자인</a>
-                <div class="sub-menu">
-    <ul>
-      <li>Menu 1</li>
-      <li>Menu 2</li>
-      <li>Menu 3</li>
-    </ul>
-  </div>
-              </li>
-            </ul>
-          </nav>
-          <div class="header-right">
-            <div class="box yellow"><a href="#"><i class="fa fa-comments"></i></a></div>
-            <div class="box blue"><a href="#"><i class="fa fa-envelope"></i></a></div>
-            <div class="box green"><a href="#"><i class="fa fa-naver"></i></a></div>
+  <header>
+    <div class="header-container">
+      <div class="logo-container">
+        <img class="logo" src="images/logo.png" />
+      </div>
+      <div class="header-right-div">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">킨더플레이룸</a>
+              <div class="sub-menu">
+                <ul>
+                  <li>Menu 1</li>
+                  <li>Menu 2</li>
+                  <li>Menu 3</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a href="#">프랜차이즈</a>
+              <div class="sub-menu">
+                <ul>
+                  <li>Menu 1</li>
+                  <li>Menu 2</li>
+                  <li>Menu 3</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a href="#">News</a>
+              <div class="sub-menu">
+                <ul>
+                  <li>Menu 1</li>
+                  <li>Menu 2</li>
+                  <li>Menu 3</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a href="#">디자인</a>
+              <div class="sub-menu">
+                <ul>
+                  <li>Menu 1</li>
+                  <li>Menu 2</li>
+                  <li>Menu 3</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a href="#">회원메뉴</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="header-right">
+          <div class="box yellow">
+            <a href="#"><i class="fa fa-comments"></i></a>
+          </div>
+          <div class="box blue">
+            <a href="#"><i class="fa fa-envelope"></i></a>
+          </div>
+          <div class="box green">
+            <a href="#"><i class="fa fa-naver"></i></a>
           </div>
         </div>
       </div>
-    </header>
+    </div>
+  </header>
 </template>
 
-<style>
+<style lang="scss">
 .header-container {
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  height: 14rem;
+  height: 12rem;
 }
 
-.header-container > div:nth-child(1) {
+.logo-container {
   .logo {
     margin-left: 4rem;
     width: 8rem;
     height: auto;
   }
+}
 
-  nav {
+.header-right-div {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  flex-direction: row;
+
+  > nav {
     display: flex;
     justify-content: space-between;
 
@@ -84,71 +104,51 @@
         display: inline-block;
         padding: 1rem;
 
-        > a:hover {
-          font-weight: bold;
-        }
-
-        .sub-menu {
-          display: none;
-          position: absolute;
-          top: 100%;
-          background-color: #fff;
-          border: 1px solid #000;
-          opacity: 0;
-          visibility: hidden;
-          transition: all 0.3s ease-in-out;
-          transform: translateY(-10px);
-
-          > ul {
-            background-color: white;
-            border: 1px solid black;
-            list-style: none;
-            margin: 0;
-            padding: 1rem;
-
-            > li {
-              border-bottom: 1px solid #000;
-              text-align: center;
-              position: relative;
-              display: inline-block;
-              padding: 0.5rem 0;
-            }
-          }
-        }
-
-        &:hover .sub-menu {
-          display: block;
-          opacity: 1;
-          visibility: visible;
-          transform: translateY(0);
-          animation: all 0.5s ease-in-out;
-        }
-
-        > ul {
-          list-style: none;
-          margin: 0;
-          padding: 0;
-          display: flex;
-          justify-content: space-between;
-
-          > li {
-            > a {
-              display: block;
-              padding: 1rem;
-              text-decoration: none;
-              color: black;
-            }
-          }
-        }
+        // border: dashed 2px black;
+        // background: gray;
       }
     }
   }
 }
 
-.header-container > div:nth-child(2) {
-  display: flex;
-  height: 100%;
-  align-items: center;
+.sub-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  background-color: #fff;
+  border: 1px solid #000;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.3s ease-in-out;
+  transform: translateX(-50%) translateY(10px);
+  padding: 1rem;
+  min-width: fit-content;
+
+  > ul {
+    list-style: none;
+    margin: 0;
+    padding: 1rem;
+
+    > li {
+      border-bottom: 1px solid #000;
+      text-align: center;
+      position: relative;
+      display: inline-block;
+      padding: 0.5rem 0;
+    }
+  }
+}
+
+.header-right-div a:hover {
+  font-weight: bold;
+}
+
+.header-right-div > nav > ul > li > a:hover + .sub-menu {
+  display: block;
+  left: 50%;
+  transform: translateX(-50%) translateY(10px);
+  opacity: 1;
+  visibility: visible;
 }
 
 .header-right {
@@ -177,7 +177,8 @@
   height: 100%;
 }
 
-.box {}
+.box {
+}
 
 .fa {
   font-size: 2rem;
@@ -192,5 +193,4 @@
     display: block;
   }
 }
-
 </style>
